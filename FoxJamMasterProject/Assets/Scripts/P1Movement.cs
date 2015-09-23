95e5007253f3534a1af8178b8fc2a7873d1f35e3
mp2-cole <colemarcus@hotmail.co.uk>
2015-09-23 21:44:51 +0100

    Our basic Player move
    Just move left and right , also contains player boundary setting. i.e
    set individual to level so player cant move out of map.

---
+++ FoxJamMasterProject/Assets/Scripts/P1Movement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, yMin, yMax;
+	
+}
+
+public class P1Movement : MonoBehaviour {
+	private RigidBody2D P1character;
+	public float speed = 1.0f;
+	public Boundary bounds;
+	// Use this for initialization
+	void Start () 
+	{
+		P1character = this.rigidbody2D;
+	}
+	
+   //use fixed update for physics based character
+	void FixedUpdate()
+	{
+		float moveHorizontal = input.GetAxis ("Horizontal");
+		float moveVertical = input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2(moveHorizontal,moveVertical);
+		rigidbody.velocity = movement * speed * Time.deltaTime;
+
+		rigidbody.position = new Vector2 
+		(Mathf.Clamp (rigidbody2D.position.x, bounds.xMin, bounds.xMax),
+		 Mathf.Clamp (rigidbody2D.positon.y, bounds.yMin, bounds.yMax));
+	}
+}
+
