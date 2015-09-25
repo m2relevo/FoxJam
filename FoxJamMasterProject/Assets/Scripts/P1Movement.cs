7e4f66e78cd3f54a6cbc014811afab3032ef2fa2
546576 <jw2-deering@STU-CZC3280QCL.campus.ads.uwe.ac.uk>
2015-09-25 11:49:20 +0100

    added shitty move code

--- FoxJamMasterProject/Assets/Scripts/P1Movement.cs
+++ FoxJamMasterProject/Assets/Scripts/P1Movement.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
 using System.Collections;
 
 [System.Serializable]
@@ -9,27 +9,28 @@ public class Boundary
 }
 
 public class P1Movement : MonoBehaviour {
-	private RigidBody2D P1character;
+	private Rigidbody2D P1character;
 	public float speed = 1.0f;
 	public Boundary bounds;
 	// Use this for initialization
 	void Start () 
 	{
-		P1character = this.rigidbody2D;
+		P1character = this.Rigidbody2D;
 	}
 	
    //use fixed update for physics based character
 	void FixedUpdate()
 	{
-		float moveHorizontal = input.GetAxis ("Horizontal");
-		float moveVertical = input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector2 movement = new Vector2(moveHorizontal,moveVertical);
-		rigidbody.velocity = movement * speed * Time.deltaTime;
+		Rigidbody.velocity = movement * speed * Time.deltaTime;
 
-		rigidbody.position = new Vector2 
-		(Mathf.Clamp (rigidbody2D.position.x, bounds.xMin, bounds.xMax),
-		 Mathf.Clamp (rigidbody2D.positon.y, bounds.yMin, bounds.yMax));
+		Rigidbody.position = new Vector2 
+		(Mathf.Clamp (Rigidbody2D.position.x, bounds.xMin, bounds.xMax),
+		 Mathf.Clamp (Rigidbody2D.position.y, bounds.yMin, bounds.yMax));
 	}
 }
 
+*/
