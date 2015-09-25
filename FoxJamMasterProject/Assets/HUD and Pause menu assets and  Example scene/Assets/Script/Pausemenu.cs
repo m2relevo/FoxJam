7736301d654b30066bc9f07fae218c7f7ba24581
m2relevo <m2-relevo@STU-CZC3280QD2.campus.ads.uwe.ac.uk>
2015-09-25 11:51:09 +0100

    Master Fox UI
    UI art assets set with pause menu code partially working

---
+++ FoxJamMasterProject/Assets/HUD and Pause menu assets and  Example scene/Assets/Script/Pausemenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pausemenu : MonoBehaviour
+
+{
+	public GameObject Pause;
+	private bool paused = false;
+
+	void start ()
+	{
+		Pause.SetActive (false);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape) & paused == false) 
+		{
+			Pause.SetActive (true);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape) & paused == true)
+		{
+			Pause.SetActive (false);
+		}
+	}
+
+/*
+	void update()
+	{
+		if (Input.GetButtonDown ("Pause"))
+		{
+			paused = ! paused;
+	
+		}
+	
+		if (paused)
+		{
+			Pause.SetActive(true);
+			//Time.timescale = 0;
+		}
+
+		if (!paused)
+		{
+			Pause.SetActive(false);
+			//Time.timescale = 1;
+		}
+	}
+	*/
+}
