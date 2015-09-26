63681a59171e2cbd61b3373f2a63783a2ecfa567
mp2-cole <colemarcus@hotmail.co.uk>
2015-09-26 13:34:33 +0100

    barfing stuff all over dis

---
+++ FoxJamMasterProject/Assets/Scripts/Tags.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour
+{
+    public const string player = "Player";
+    public const string camera = "MainCamera";
+    public const string gameController = "GameController";
+	
+}
