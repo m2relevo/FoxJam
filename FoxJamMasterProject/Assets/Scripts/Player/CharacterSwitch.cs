5aeb8a80140b17864444d85fbbde1633c97c8f63
mp2-cole <colemarcus@hotmail.co.uk>
2015-09-27 17:06:59 +0100

    Switch
    can now swap characters with left ctrl set so swaps cameras and so that
    only the character u control moves

---
+++ FoxJamMasterProject/Assets/Scripts/Player/CharacterSwitch.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSwitch : MonoBehaviour {
+
+
+	public Camera cam1;
+	public Camera cam2;
+	public GameObject player1;
+	public GameObject player2;
+	bool playingC;
+
+	// Use this for initialization
+	void Start () 
+	{
+		cam1.enabled = true;
+		cam2.enabled = false;
+		playingC = false;
+		player1.GetComponent<P1move>().enabled = true;
+		player2.GetComponent<P2move>().enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+
+			if (Input.GetKeyDown(KeyCode.LeftControl)) 
+			{
+				playingC = !playingC;
+				playerSwitch (playingC);
+			}
+	}
+	void playerSwitch(bool Switch)
+	{
+		if (Switch == true) 
+		{
+			player1.GetComponent<P1move>().enabled = false;
+			player2.GetComponent<P2move>().enabled = true;
+			cam1.enabled = false;
+			cam2.enabled = true;
+		}
+		if (Switch == false) 
+		{
+			player1.GetComponent<P1move>().enabled = true;
+			player2.GetComponent<P2move>().enabled = false;
+			cam2.enabled = false;
+			cam1.enabled = true;
+		}
+	}
+}
