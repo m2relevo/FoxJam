e3b796690f9497202b2baa24824a2b0af5af3ebd
mp2-cole <colemarcus@hotmail.co.uk>
2015-09-27 22:40:14 +0100

    lever and pads aswell as obstacle script
    script to detect user and  or input. Script to cause obstacles to
    dissapear when desired condition met.

---
+++ FoxJamMasterProject/Assets/Scripts/Doors.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Doors : MonoBehaviour 
+{
+
+
+	public void DoorGone ()
+	{
+
+			this.GetComponent<MeshRenderer>().enabled = false;
+			this.GetComponent<BoxCollider2D>().enabled = false;
+
+	}
+
+	public void DoorHere ()
+	{
+		this.GetComponent<MeshRenderer>().enabled = true;
+		this.GetComponent<BoxCollider2D>().enabled = true;
+	}
+}
