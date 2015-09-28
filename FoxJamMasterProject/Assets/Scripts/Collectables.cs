6471c1ab06ef9d7512064c25b99a6a88f51cd370
Popesture <g2-pope@STU-CZC3280QCW.campus.ads.uwe.ac.uk>
2015-09-28 15:08:13 +0100

    Collectabe Script
    Fading sprites + collectable score/destroy

---
+++ FoxJamMasterProject/Assets/Scripts/Collectables.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectables : MonoBehaviour 
+{
+	public float max = 1f;
+	public float speed = 5.0f;	
+	public SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		sprite.color = new Color(1f, 1f, 1f, Mathf.PingPong(Time.time * speed, max));
+	}
+
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		ScoreManager.score = + 100;
+		//renderer.gameObject.color.a = 0;
+		if (other.tag == "Player") 
+		{
+			Destroy(gameObject);
+		}
+	
+	}
+}
---
+++ FoxJamMasterProject/Assets/Scripts/FadeScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeScript : MonoBehaviour {
+
+	public float max = 1f;
+	public float speed = 5.0f;
+	
+	private SpriteRenderer sprite;
+
+	void Start()
+	{
+	   sprite =	this.gameObject.GetComponent<SpriteRenderer>();
+	}
+
+	void Update () {
+		sprite.color = new Color(1f, 1f, 1f, Mathf.PingPong(Time.time * speed, max));
+	}
+}
