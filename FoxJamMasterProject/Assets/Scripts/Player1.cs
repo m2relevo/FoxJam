caa9f2919df9e059db1d30ccbc2bbdfb027a7ca4
Rambo3592 <rambo3592@googlemail.com>
2015-09-28 14:52:07 +0100

    Player

--- FoxJamMasterProject/Assets/Scripts/Player1.cs
+++ FoxJamMasterProject/Assets/Scripts/Player1.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Player1 : MonoBehaviour {
 	
-	public float maxSpeed = 3f;
+	public float maxSpeed = 3;
 	public float speed = 50f;
 	public float jumpPower = 200f;
 	
@@ -24,22 +24,20 @@ public class Player1 : MonoBehaviour {
 	{
 		anim.SetBool ("Grounded", grounded);
 		anim.SetFloat ("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
-
-
-        if (grounded && Input.GetKeyDown(KeyCode.W))
-        {
-            rb2d.AddForce(Vector2.up * jumpPower * Time.deltaTime); 
-        }
-        if (Input.GetAxis ("Horizontal") < 0f)
+		
+		if (Input.GetAxis ("Horizontal") < 0f)
 		{
-            Debug.Log("Button was pressed");
-            transform.localScale = new Vector3(1,1,1);
+			transform.localScale = new Vector3(1,1,1);
 		}
 		if (Input.GetAxis ("Horizontal") > -0f) 
 		{
 			transform.localScale = new Vector3(-1,1,1);
 		}
 		
+		if (Input.GetButtonDown ("Jump") && grounded)
+		{
+			rb2d.AddForce(Vector2.up * jumpPower);
+		}
 	}
 	
 	void FixedUpdate()
