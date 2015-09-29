2a6e5d329381506424910fea968ec3ba2639a608
Rambo3592 <ahd2-rambaut@STU-CZC3280QDF.campus.ads.uwe.ac.uk>
2015-09-29 12:52:38 +0100

    pls sync
    420 goty

--- FoxJamMasterProject/Assets/Scripts/GroundCheck2.cs
+++ FoxJamMasterProject/Assets/Scripts/GroundCheck2.cs
@@ -1,25 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GroundCheck : MonoBehaviour {
-
-	private Player1 player;
-
+public class GroundCheck2 : MonoBehaviour {
+	
+	private Player2 player;
+	
 	void Start()
 	{
-		player = gameObject.GetComponentInParent <Player1> ();
+		player = gameObject.GetComponentInParent <Player2> ();
 	}
 	
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		player.grounded = true;
 	}
-
+	
 	void OnTriggerStay2D(Collider2D col)
 	{
 		player.grounded = true;
 	}
-
+	
 	void OnTriggerExit2D(Collider2D col)
 	{
 		player.grounded = false;
